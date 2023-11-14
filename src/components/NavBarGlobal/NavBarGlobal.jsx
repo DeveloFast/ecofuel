9206b2f36c8e661fa0d9dafaf97f97dd4327207a
santiagor06 <santiago.restrepo06@hotmail.com>
2023-11-14 18:53:25 -0500

    nav-change

--- src/components/NavBarGlobal/NavBarGlobal.jsx
+++ src/components/NavBarGlobal/NavBarGlobal.jsx
@@ -6,12 +6,10 @@ function NavBarGlobal(){
         <div className="menu-global">
             <img src={logo} alt="" className='logo'/>
             <div className='botones'>               
-            </div>
-            <div>
             <input className='button-direction' type="button" value="Encuéntranos"/>
             <button className='button-home'><img src="https://us.123rf.com/450wm/yuriizasimov/yuriizasimov2006/yuriizasimov200600086/149147484-imagen-gris-de-perfil-de-avatar-de-marcador-de-posici%C3%B3n-predeterminado-masculino-aislada-sobre-fondo.jpg?ver=6" alt="" className='predeterminado'/></button>
-
             </div>
+  
         </div>
     </>)
 }
