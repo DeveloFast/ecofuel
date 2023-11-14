34f64e8a181a18fa609c6e503cea17d473eb2a57
Santiagotamayoz <tamayozuluagas>
2023-11-14 15:28:04 -0500

    NavBar

--- src/components/NavBarGlobal/NavBarGlobal.jsx
+++ src/components/NavBarGlobal/NavBarGlobal.jsx
@@ -3,8 +3,12 @@ import '../NavBarGlobal/NavBarGlobal.css'
 
 function NavBarGlobal(){
     return(<>
-        <div className="menu-cafe">
-            <img src={logo} alt="" />
+        <div className="menu-global">
+            <img src={logo} alt="" className='logo'/>
+            <div className='botones'>               
+            </div>
+            <input className='button-direction' type="button" value="Direccion"/>
+            <button className='button-home'><img src="https://us.123rf.com/450wm/yuriizasimov/yuriizasimov2006/yuriizasimov200600086/149147484-imagen-gris-de-perfil-de-avatar-de-marcador-de-posici%C3%B3n-predeterminado-masculino-aislada-sobre-fondo.jpg?ver=6" alt="" className='predeterminado'/></button>
         </div>
     </>)
 }
